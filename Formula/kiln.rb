705bf1da78fc018751b7e54c09f451a9491b9102
tmk <tmk@exidia.com>
2026-06-13 13:49:03 +0200

    kiln 2026.06.13.01

--- Formula/kiln.rb
+++ Formula/kiln.rb
@@ -1,23 +1,23 @@
 class Kiln < Formula
   desc "Template compiler for GFI (gulp-file-include) syntax"
   homepage "https://github.com/teehemkay/homebrew-kiln"
-  version "2026.06.10.01"
+  version "2026.06.13.01"
 
   on_macos do
     on_arm do
-      url "https://github.com/teehemkay/homebrew-kiln/releases/download/2026.06.10.01/kiln-2026.06.10.01-darwin-arm64"
-      sha256 "e8ddee65f2954bdc85a8768595f858bce66cff05af84ecbc94ff05f45f81da46"
+      url "https://github.com/teehemkay/homebrew-kiln/releases/download/2026.06.13.01/kiln-2026.06.13.01-darwin-arm64"
+      sha256 "dd41e57e9a17b6439704aad741312caa844bdb85aa2a7174b5d44db84184013d"
     end
     on_intel do
-      url "https://github.com/teehemkay/homebrew-kiln/releases/download/2026.06.10.01/kiln-2026.06.10.01-darwin-x64"
-      sha256 "da21faf6bab6ea8f31a05b62f5d24dbc510851a2ac87a2630e8c9da28270283d"
+      url "https://github.com/teehemkay/homebrew-kiln/releases/download/2026.06.13.01/kiln-2026.06.13.01-darwin-x64"
+      sha256 "8628afb37466897ec3bbb8792aeabe66bcfb2b9cda358006448827028f991080"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/teehemkay/homebrew-kiln/releases/download/2026.06.10.01/kiln-2026.06.10.01-linux-x64"
-      sha256 "638075cb03649b430ab469fe097ffa3af1d6ea54547bc829d0285cabed4f1470"
+      url "https://github.com/teehemkay/homebrew-kiln/releases/download/2026.06.13.01/kiln-2026.06.13.01-linux-x64"
+      sha256 "b55f74e5a73b39d8819354f24e17682c4e518db342d42ecab132d9121b5120fe"
     end
   end
 
